1d43e5b62ee11747888c53edf13498ce05784e19
samtarlo3011 <samuelt136@nycstudents.net>
2026-02-26 10:25:31 -0500

    Update pet.py

--- pet.py
+++ pet.py
@@ -15,8 +15,11 @@ class pet:
         self.name = name
 pet.hp = 100
 pet.hungry = "low"
+h=0
 pet.clean = "clean" 
+c=10
 pet.energy = "high"
+e=10
 import random
 g=random.randint(1,2)
 if g == 1:
@@ -41,6 +44,7 @@ elif want == 4:
 elif want == 5:
     pet.species = "Bird"
 pet.love = 0
+l=0
 sp = random.randint(1,10)
 st = random.randint(1,10)
 si = random.randint(1,10)
@@ -48,6 +52,7 @@ pet.speed = sp
 pet.strength = st
 pet.size = si
 pet.age = 0
+a=0
 name=input("""name your pet!
 """)
 """ print(pet.hp,pet.hungry,pet.clean,pet.energy,pet.gender,pet.species,pet.love,pet.speed,pet.strength,pet.size,pet.age) """
@@ -71,3 +76,41 @@ if  pet.age<5:
 14.train pet strength
 15.visit the vet
 16.go to sleep for the night"""))
+if action == 1:
+    print(pet.hungry)
+elif action == 2:
+    print(pet.energy)
+elif action == 3:
+    print(pet.love)
+elif action == 4:
+    print(pet.hp)
+elif action == 5:
+    print(pet.clean)
+elif action == 6:
+    print(pet.speed)
+elif action == 7:
+    print(pet.strength)
+elif action == 8:
+    print(pet.size)
+elif action == 9:
+    print(pet.age)
+elif action == 10:
+    h=h+5
+    e=e-1
+elif action == 11:
+    l=l+5
+    e=e-1
+elif action == 12:
+    c=c+5
+    e=e-1
+elif action == 13:
+    sp=sp+5
+    e=e-3
+elif action == 14:
+    st=st+5
+    e=e-3
+elif action == 15:
+    "this is visiting the vet i need to figure out what to do about this"
+elif action == 16:
+    a=a+1
+    e=e+5

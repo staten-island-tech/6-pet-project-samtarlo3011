3b6fea8c105ccb187be30cb75ae0d241f9c38275
samtarlo3011 <samuelt136@nycstudents.net>
2026-03-23 10:17:06 -0400

    Update annoying_functions.py

--- annoying_functions.py
+++ annoying_functions.py
@@ -2,7 +2,7 @@ import time
 import random
 pets=[]
 petsN=[]
-store = 0
+storeW = 0
 class pet:
     def __init__(self, hungry, clean, energy, gender, species, hp, love, production, speed, strength, size, age,name,money,h,c,e,l,a,ag,):
         self.hp = hp 
@@ -79,25 +79,38 @@ class pet:
         pets.append(pet(hungry, clean, energy, gender, species, hp, love, production, speed, strength, size, age,name,money,h,c,e,l,a,ag))
         return pet(hungry, clean, energy, gender, species, hp, love, production, speed, strength, size, age,name,money,h,c,e,l,a,ag)
     def petstore():
-        store1 = pet.New_Pet(1)
+        storeW=1
+        store1=pet.New_Pet(1)
         store2=pet.New_Pet(1)
         store3=pet.New_Pet(1)
         store4=pet.New_Pet(1)
         store5=pet.New_Pet(1)
         store = [
-        {"id" :"1" , "species" : store1.species , "strength" : store1.strength , "speed" : store1.speed , "gender" : store1.gender,},
-        {"id" :"2", "species" : store2.species,"strength" : store2.strength,"speed" : store2.speed,"gender" : store2.gender,},
-        {"id" :"3", "species" : store3.species,"strength" : store3.strength,"speed" : store3.speed,"gender" : store3.gender,},
-        {"id" :"4", "species" : store4.species,"strength" : store4.strength,"speed" : store4.speed,"gender" : store4.gender,},
-        {"id" :"5", "species" : store5.species,"strength" : store5.strength,"speed" : store5.speed,"gender" : store5.gender,},
+        {"id" :1, "species" : store1.species,"strength" : store1.strength,"speed" : store1.speed,"gender" : store1.gender,},
+        {"id" :2, "species" : store2.species,"strength" : store2.strength,"speed" : store2.speed,"gender" : store2.gender,},
+        {"id" :3, "species" : store3.species,"strength" : store3.strength,"speed" : store3.speed,"gender" : store3.gender,},
+        {"id" :4, "species" : store4.species,"strength" : store4.strength,"speed" : store4.speed,"gender" : store4.gender,},
+        {"id" :5, "species" : store5.species,"strength" : store5.strength,"speed" : store5.speed,"gender" : store5.gender,},
         ]
         for i in store:
-            want=int(input("enter the id of what pet you would like to buy. If no pet is wanted enter 6"))
-            if want == i["id"]:
-                print("congrats on purchasing a new", i["species"], ("thatll be $20 and you can return to the rest of the shop."))
+            print("id:",i["id"],",","species:",i["species"],",","strength:",i["strength"],",","speed:",i["speed"],",","gender:",i["gender"])
+        want=int(input("enter the id of what pet you would like to buy. If no pet is wanted enter 6: "))
+        if want in range(1,7):
+            if want == 6:
+                print("return")
+            for i in store:
+                if want == i["id"]:
+                    print("congrats on purchasing a new", i["species"], "thatll be $20!")
+                    name=input("Now, name your new pet: " )
+                    petsN.append(name)
+                    print([i])
+                    pets.append(store[[i][0]["id"]])
+        else:
+            print("please enter a valid id")
+            
 
 pet1 = pet.New_Pet(0)
-pet.petstore
+pet.petstore()
+print(pets,petsN)
 
-print (store[0],store[1], store[2], store[3], store[4], sep="\n")
 
